77f8e2d33b1fb138162a14a9bda751cf5a54e784
Yhuan Shin <yhuanft@stpeter.com.ph>
2026-06-24 16:49:50 +0800

    fix: update page title from "St. Peter eStore" to "1SP eStore"

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <title>St. Peter eStore</title>
+        <title>1SP eStore</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
